163e83734e94045911bfc18ad27d7aca945e5ce6
Игорь Боднар <bodnar_i@mail.ru>
2015-06-18 17:35:47 +0600

    Change composer.json

---
+++ tests/HookTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ibodnar
+ * Date: 18.06.15
+ * Time: 17:10
+ */
+
+namespace AmaxLab\GitWebHook\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class HookTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testExecute()
+    {
+
+    }
+}
